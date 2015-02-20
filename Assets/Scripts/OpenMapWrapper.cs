9800e30f8f01bc6128718286e5ad73f7a8e61c16
Emmanuel Ariza <ejariza@gmail.com>
2015-02-20 11:29:46 -0500

    Se agregó la carpeta de scripts

---
+++ Assets/Scripts/OpenMapWrapper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenMapWrapper : MonoBehaviour, MapWrapper {
+
+
+	public string GetCoordinates(){
+		string coordinates = "Hit (Guess what this is)";
+		Debug.Log (coordinates);
+		return coordinates;
+	
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
