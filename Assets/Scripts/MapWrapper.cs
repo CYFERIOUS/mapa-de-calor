d4b6b89ed0a483a6ee9b7784618b55d81d3c5af0
AndresDiaz1 <andiazvar@gmail.com>
2015-02-24 11:48:49 -0500

    Coloca el valor de coordenada en el textbox
    Colca el valor de la coordenada en el inputfield DirectionInputField, de
    la ventana de ReportWindow

--- Assets/Scripts/MapWrapper.cs
+++ Assets/Scripts/MapWrapper.cs
@@ -6,4 +6,5 @@ using System.Collections.Generic;
 interface MapWrapper{
 	Dictionary<string, double> GetCursorCoordinates();
 	void SetMarkerInMap();
+	void SetCoordinatesOnInputField(double latitude, double longitude);
 }
--- Assets/Scripts/OpenMapWrapper.cs
+++ Assets/Scripts/OpenMapWrapper.cs
@@ -2,11 +2,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-
+using UnityEngine.UI;
 
 public class OpenMapWrapper : MonoBehaviour, MapWrapper
 {
 	public TestMap testMap;
+	public InputField DirectionInputField;
 
 	public Dictionary<string, double> GetCursorCoordinates ()
 	{
@@ -18,9 +19,12 @@ public class OpenMapWrapper : MonoBehaviour, MapWrapper
 		Debug.Log ("Click en la clase del wrapper");
 		Dictionary<string, double> CursorCoordinates = GetCursorCoordinates ();
 		testMap.CreateAnnotationOnClick(CursorCoordinates["latitude"], CursorCoordinates["longitude"]);
-
+		SetCoordinatesOnInputField (CursorCoordinates["latitude"],CursorCoordinates["longitude"]);
 	}
-	
+
+	public void SetCoordinatesOnInputField(double latitude, double longitude){
+		DirectionInputField.text = latitude.ToString () + " , " + longitude.ToString ();
+	}
 	// Use this for initialization
 	void Start ()
 	{
