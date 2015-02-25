a567f23da5ebb88cd86c59278a887489cc56995d
DeviaFernando <deviafernando@hotmail.com>
2015-02-25 12:18:13 -0500

    CLASS SAVER-PREFS WITH PROOFS

--- Assets/Scripts/Data/ReportSaver.cs
+++ Assets/Scripts/Data/ReportSaver.cs
@@ -9,14 +9,14 @@ public class ReportSaver {
 	private int timeStamp;
 	private string userName;
 	private Vector2 location;
-	private Robbery robbery;
+	private int robbery;
 	private string article;
 	private string comments;
 
 	public ReportSaver(){
 
 		location = new Vector2();
-		robbery = Robbery.non_specified;
+		robbery = (int)Robbery.non_specified;
 	
 	}
 
@@ -24,38 +24,80 @@ public class ReportSaver {
 		return this.location;
 	}
 
-	public Robbery getRobbery() {
+	public int getRobbery() {
 		return this.robbery;
 	}
 
-	private void saverPrefs(){
+	public int getTimeStamp(){
+		return this.timeStamp;
+	}
 
+	public string getUserName(){
+		return this.userName;
+	}
+
+	public string getArticle(){
+		return this.article;
+	}
+
+	public string getComments(){
+		return this.comments;
+	}
+
+	public void setLocation(Vector2 location) {
+		this.location = location;
+	}
+	
+	public void setRobbery(int robbery) {
+		this.robbery = robbery;
+	}
+	
+	public void setTimeStamp(int timeStamp){
+		this.timeStamp = timeStamp;
+	}
+	
+	public void setUserName(string userName){
+		this.userName = userName;
+	}
+	
+	public void setArticle(string article){
+		this.article = article;
+	}
+	
+	public void setComments(string comments){
+		this.comments = comments;
+	}
+
+	public void save(){
 		PlayerPrefs.SetInt("time", timeStamp);
 		PlayerPrefs.SetString("user", userName);
 		PlayerPrefs.SetFloat ("locationx", location.x);
 		PlayerPrefs.SetFloat ("locationy", location.y);
-		//PlayerPrefs.SetString ("robbing", robbery);
+		PlayerPrefs.SetInt ("robbing", robbery);
 		PlayerPrefs.SetString ("stuff", article);
 		PlayerPrefs.SetString ("description", comments);
+		PlayerPrefs.Save ();
 	}
 
-	private void loadPrefs(){
-		
-		PlayerPrefs.GetInt("time", timeStamp);
-		PlayerPrefs.GetString("user", userName);
-		PlayerPrefs.GetFloat ("locationx", location.x);
-		PlayerPrefs.GetFloat ("locationy", location.y);
-		//PlayerPrefs.GetString ("robbing", robbery);
-		PlayerPrefs.GetString ("stuff", article);
-		PlayerPrefs.GetString ("description", comments);
+	public void load(){
+		this.setTimeStamp(PlayerPrefs.GetInt("time", timeStamp));
+		this.setUserName(PlayerPrefs.GetString("user", userName));
+		this.setRobbery(PlayerPrefs.GetInt ("robbing", robbery));
+		this.setArticle(PlayerPrefs.GetString ("stuff", article));
+		this.setComments(PlayerPrefs.GetString ("description", comments));
+
+		Vector2 loc = new Vector2 ();
+		loc.x = PlayerPrefs.GetFloat ("locationx", location.x);
+		loc.y = PlayerPrefs.GetFloat ("locationy", location.y);
+		this.setLocation(loc);
 	}
 
 }
 
 	
 public enum Robbery{
-	non_specified,
-	Assault,
-	Thievery
+	non_specified = 0,
+	Assault = 1,
+	Thievery = 2
 }
 
--- Assets/Scripts/Test/Editor/ReportSaverTests.cs
+++ Assets/Scripts/Test/Editor/ReportSaverTests.cs
@@ -5,6 +5,7 @@ using NUnit.Framework;
 using UnityEngine;
 
 namespace UnityTest {
+
 	[TestFixture]
 	[Category("Report Saver Tests")]
 	internal class ReportSaverTests {
@@ -18,7 +19,37 @@ namespace UnityTest {
 			Assert.AreEqual (saver.getLocation().x, location.x);
 			Assert.AreEqual (saver.getLocation().y, location.y);
 
-			Assert.AreEqual (saver.getRobbery(), Robbery.non_specified);
+			Assert.AreEqual (saver.getRobbery(), (int)Robbery.non_specified);
+		}
+
+		[Test]
+		[Category("PalyerPrefs Test")]
+		public void playerPrefTest() {
+			ReportSaver saver = new ReportSaver ();
+			ReportSaver saverToCompare = new ReportSaver ();
+
+			Vector2 location = new Vector2 ();
+			location.x = 0.5854f;
+			location.y = 98.77f;
+
+			saver.setArticle ("Article test");
+			saver.setComments ("Comment Test");
+			saver.setUserName ("User Test");
+			saver.setLocation (location);
+			saver.setRobbery ((int)Robbery.Assault);
+			saver.setTimeStamp (1254874);
+		
+
+			saver.save ();
+			saverToCompare.load ();
+
+			Assert.AreEqual (saver.getTimeStamp(), saverToCompare.getTimeStamp());
+			Assert.AreEqual (saver.getUserName(), saverToCompare.getUserName());
+			Assert.AreEqual (saver.getLocation().x, saverToCompare.getLocation().x);
+			Assert.AreEqual (saver.getLocation().y, saverToCompare.getLocation().y);
+			Assert.AreEqual (saver.getArticle(), saverToCompare.getArticle());
+			Assert.AreEqual (saver.getComments(), saverToCompare.getComments());
+
 		}
 	}
 }
