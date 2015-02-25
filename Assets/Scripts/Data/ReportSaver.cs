bb39aa0f4407ed70d1ecd3a2a977ba95f7ed26ef
DeviaFernando <deviafernando@hotmail.com>
2015-02-25 10:55:33 -0500

    Se agregan pruebas a la clase ReportSaver; las pruebas verifican que reportSaver inicialice bien

---
+++ Assets/Scripts/Data/ReportSaver.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class ReportSaver {
+
+	private int timeStamp;
+	private string userName;
+	private Vector2 location;
+	private Robbery robbery;
+	private string article;
+	private string comments;
+
+	public ReportSaver(){
+
+		location = new Vector2();
+		robbery = Robbery.non_specified;
+	
+	}
+
+	public Vector2 getLocation() {
+		return this.location;
+	}
+
+	public Robbery getRobbery() {
+		return this.robbery;
+	}
+
+	private void saverPrefs(){
+
+		PlayerPrefs.SetInt("time", timeStamp);
+		PlayerPrefs.SetString("user", userName);
+		PlayerPrefs.SetFloat ("locationx", location.x);
+		PlayerPrefs.SetFloat ("locationy", location.y);
+		//PlayerPrefs.SetString ("robbing", robbery);
+		PlayerPrefs.SetString ("stuff", article);
+		PlayerPrefs.SetString ("description", comments);
+	}
+
+	private void loadPrefs(){
+		
+		PlayerPrefs.GetInt("time", timeStamp);
+		PlayerPrefs.GetString("user", userName);
+		PlayerPrefs.GetFloat ("locationx", location.x);
+		PlayerPrefs.GetFloat ("locationy", location.y);
+		//PlayerPrefs.GetString ("robbing", robbery);
+		PlayerPrefs.GetString ("stuff", article);
+		PlayerPrefs.GetString ("description", comments);
+	}
+
+}
+
+	
+public enum Robbery{
+	non_specified,
+	Assault,
+	Thievery
+}
+
---
+++ Assets/Scripts/Test/Editor/ReportSaverTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityTest {
+	[TestFixture]
+	[Category("Report Saver Tests")]
+	internal class ReportSaverTests {
+
+		[Test]
+		[Category("Initialization Test")]
+		public void initializationTest() {
+			ReportSaver saver = new ReportSaver ();
+
+			Vector2 location = new Vector2 ();
+			Assert.AreEqual (saver.getLocation().x, location.x);
+			Assert.AreEqual (saver.getLocation().y, location.y);
+
+			Assert.AreEqual (saver.getRobbery(), Robbery.non_specified);
+		}
+	}
+}
