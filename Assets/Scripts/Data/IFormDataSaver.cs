6aa3a995304f9f282aea44c61cdef9c4b1953ac1
AndresDiaz1 <andiazvar@gmail.com>
2015-02-27 11:32:52 -0500

    Eliminar data

--- Assets/Scripts/Data/IFormDataSaver.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IformSaver  {
-
-	void Save(string val, bool isName);
-	void SetStorage(IDataStorage storage);
-	void SaveVector(Vector2 storage);
-}
