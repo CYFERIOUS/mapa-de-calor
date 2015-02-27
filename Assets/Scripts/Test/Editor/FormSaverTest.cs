452ef95fc0fd2475d4d9b9d0939eb161e0e6504c
AndresDiaz1 <andiazvar@gmail.com>
2015-02-27 12:09:52 -0500

    Correccion pruebas

--- Assets/Scripts/Test/Editor/FormSaverTest.cs
+++ Assets/Scripts/Test/Editor/FormSaverTest.cs
@@ -9,8 +9,8 @@ namespace UnityTest {
 	[Category("Report Saver Tests")]
 	internal class FormSaverTest {
 
-		IFormDataSaver saver = new DataStorageMock();
-		IDataStorage storage = new FormSaverMock();
+		IFormDataSaver saver = new FormSaverMock();
+		IDataStorage storage = new DataStorageMock();
 		FormData data = new FormData();
 
 		string name = "name";
