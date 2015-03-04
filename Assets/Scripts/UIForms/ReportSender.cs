2cdf0fa6befbe64331dcd6f56838fc0e8488b994
CYFERIOUS <frankdrujes@gmail.com>
2015-03-04 17:03:09 -0500

    SENDER_TEST_WORKING

--- Assets/Scripts/UIForms/ReportSender.cs
+++ Assets/Scripts/UIForms/ReportSender.cs
@@ -8,24 +8,56 @@ using UnityEngine;
 
 public class ReportSender: MonoBehaviour {
 
-	public InputField place;
+	public InputField ubicationField;
+	public InputField commentsField;
+	public Button submitButton;
 
+	public 
 
-	public ReportSaver saver;
-	private FormData data = new FormData ();
+	void Start(){
+		submitButton.onClick.AddListener (delegate {
+			HandleSubmitClicked();
+		});
+	}
 
-	// Use this for initialization
-	void Start () {
+	private void HandleSubmitClicked ()
+	{
+		FormData formdata = CreateFormData ();
+		ReportSaver saver = new ReportSaver();
+		saver.SetStorage (new PlayerPrefStorage());
+		saver.Save (formdata);
 
+	}
+
+	private FormData CreateFormData ()
+	{
+		 FormData data = new FormData ();
+		data.annotation = GetAnnotation ();
+		data.comments = commentsField.text;
+
+		return data;
+	}
+
+	private Vector2 GetAnnotation ()
+	{
+		string ubicationText = ubicationField.text;
+		string[] coordinates = ubicationText.Split (new string[1]{" , "}, StringSplitOptions.None);
+		return new Vector2 (float.Parse(coordinates[0]), float.Parse(coordinates[1]));
+	}
+
+	public  DateTime ConvertFromUnixTimestamp(double timestamp)
+	{
+		DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+		return origin.AddSeconds(timestamp);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public static double ConvertToUnixTimestamp(DateTime date)
+	{
+		DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+		TimeSpan diff = date.ToUniversalTime() - origin;
+		Debug.Log (Math.Floor(diff.TotalSeconds));
+		return Math.Floor(diff.TotalSeconds);
 	}
 
-	public string GetPlaceIpunt(){
-		Debug.Log (place.text);
-		return place.text;
-	}
+
 }
