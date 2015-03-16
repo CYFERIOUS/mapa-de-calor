aff5a8f5801de5aa756931a50e4b17dca8a01bd6
AndresDiaz1 <andiazvar@gmail.com>
2015-03-16 10:55:07 -0500

    pruebas formulario

---
+++ Assets/Scripts/UserReports/ReportFormValidator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReportFormValidator  {
+
+	public bool IsValid {
+		get{
+			const int FIRST_DAY_OF_MONTH = 1;
+			const int LAST_DAY_OF_MONTH = 31;
+			return Day>=1 && Day<=31;
+		}
+	}
+
+	public int Day {
+		get;
+		set;	
+	}
+
+}
