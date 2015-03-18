509c2ba8086bf7b1f48860af6551583f556da0e1
Emmanuel Ariza <ejariza@gmail.com>
2015-03-18 12:37:24 -0500

    MapWrapperBehaviour ahora es independiente de OpenMapWrapper

--- Assets/Scripts/MapWrapper/MapWrapperBehaviour.cs
+++ Assets/Scripts/MapWrapper/MapWrapperBehaviour.cs
@@ -1,31 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnitySlippyMap;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
-public class MapWrapperBehaviour:OpenMapWrapper
+public class MapWrapperBehaviour:MonoBehaviour
 {
+	public Texture	LocationTexture;
+	public Texture	MarkerTexture;
+	public GameObject ReportTrigger;
+	public InputField DirectionInputField;
+
+	protected InputReader inputReader;
+	protected double PrivateTriggerMovementManager;
+	protected bool isOnMainWindow = true;
 
 	private MapWrapper mapWrapper;
 	private AbstractMap mapImplementation;
 	private MarkerGenerator markerGenerator;
 	private IFormDataLoader loader;
 	private AnnotationManager manager;
+	public Dictionary<string, double> LastPutMarkerCoordinates = null;
+
+	private Map map;
+
 
 	void Start ()
 	{	
 		map = Map.Instance;
 		mapImplementation = new MapImplementation ();
 		mapWrapper = new MapWrapper ();
-		markerGenerator = new ConcreteMarkerGenerator (Map.Instance);
+		SetupMarkerGenerator ();
+
 		mapWrapper.MapImplementation = mapImplementation;
 		mapWrapper.MarkerGenerator = markerGenerator;
 		SetupMapInstance ();	
-		DrawGPSUserLocation ();
 		SetUpInputReader ();
-		PrivateTriggerMovementManager = map.CenterWGS84 [0];
 		SetUpLoader();
 		SetUpManager ();
 		manager.loadAnnotations();
+		PrivateTriggerMovementManager = map.CenterWGS84 [0];
 	}
 
 	void SetUpManager () {
@@ -45,20 +59,20 @@ public class MapWrapperBehaviour:OpenMapWrapper
 
 
 
-//	void Update ()
-//	{
-//		if (isOnMainWindow == true) {
-//			inputReader.Update ();
-//		}
-//		if (Input.GetKeyDown (KeyCode.Escape)) {
-//			Application.Quit ();
-//		}
-//		
-//		if (PrivateTriggerMovementManager != map.CenterWGS84 [0] && ReportTrigger.activeInHierarchy == true) {
-//			ReportTrigger.SetActive (false);
-//			RemoveLastPutMarker ();
-//		}
-//	}
+	void Update ()
+	{
+		if (isOnMainWindow == true) {
+			inputReader.Update ();
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+		
+		if (PrivateTriggerMovementManager != map.CenterWGS84 [0] && ReportTrigger.activeInHierarchy == true) {
+			ReportTrigger.SetActive (false);
+			RemoveLastPutMarker ();
+		}
+	}
 
 	public void SetupMapInstance ()
 	{	
@@ -69,5 +83,109 @@ public class MapWrapperBehaviour:OpenMapWrapper
 		mapWrapper.EnableUseLocation ();
 		mapWrapper.addInputDelegateKeyboard ();
 		mapWrapper.EnableInputs ();
+	}
+
+	public void SetupMarkerGenerator ()
+	{
+		markerGenerator = new ConcreteMarkerGenerator (Map.Instance);
+		markerGenerator.DefaultTexture = MarkerTexture;
+	}
+
+	public void SetUpInputReader(){
+		inputReader = new InputReader ();
+		inputReader.SetGenerator (GetValidInputGenerator());
+		inputReader.LongPressExecuted +=()=>{
+			SetSingleMarkerOnMap();
+			ReportTrigger.SetActive (true);
+			PrivateTriggerMovementManager = map.CenterWGS84 [0];
+		};
+	}
+
+	
+	public void RemoveLastPutMarker ()
+	{
+		RemoveMarker (LastPutMarkerCoordinates ["longitude"],LastPutMarkerCoordinates ["latitude"]);
+	}
+
+	public void RemoveMarker(double latitude, double longitude){
+		Marker[] markers = map.GetComponentsInChildren<Marker> ();
+		foreach (Marker marker in markers) {
+			if (marker.CoordinatesWGS84 [0] == latitude && marker.CoordinatesWGS84 [1] == longitude) {
+				map.RemoveMarker (marker);
+				SetNullLastPutMarkerCoordinates ();
+			}
+		}
+	}
+
+	public void SetNullLastPutMarkerCoordinates ()
+	{
+		LastPutMarkerCoordinates = null;
+	}
+
+	private InputGenerator GetValidInputGenerator()
+	{
+		#if UNITY_EDITOR
+		return GetComponent<EditorInputGenerator>();
+		#else
+		return GetComponent<DeviceInputGenerator>();
+		#endif
+	}
+
+	public void SetSingleMarkerOnMap ()
+	{
+		if (LastPutMarkerCoordinates != null) {
+			RemoveLastPutMarker ();
+			
+		}
+		SetMarkerInMap ();
+	}
+
+	public void SetMarkerInMap ()
+	{	
+		Dictionary<string, double> CursorCoordinates = GetCoordinatesOfCursor ();
+		CreateAnnotation (CursorCoordinates ["latitude"], CursorCoordinates ["longitude"]);
+		SetCoordinatesOnInputField (CursorCoordinates ["latitude"], CursorCoordinates ["longitude"]);
+		LastPutMarkerCoordinates = CursorCoordinates;
+
+	}
+
+	public Dictionary<string, double> GetCoordinatesOfCursor ()
+	{
+		Dictionary<string, double> dictionary = new Dictionary<string, double> ();
+		
+		Vector3 wordPos = getCursorPosition ();
+		
+		//ReportTrigger.transform.localScale = new Vector2 (0.5f,0.5f);
+		double latitude = (0.0167 * wordPos [2]) + ((map.CenterWGS84) [1]);
+		double longitude = (0.0167 * wordPos [0]) + ((map.CenterWGS84) [0]);
+		dictionary.Add ("latitude", latitude);
+		dictionary.Add ("longitude", longitude);
+		return dictionary;
+		
+	}
+
+	public Vector3 getCursorPosition ()
+	{
+		Vector3 mousePos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0f);
+		ReportTrigger.transform.position = mousePos;
+		Ray ray = Camera.main.ScreenPointToRay (mousePos);
+		RaycastHit hit;
+		
+		Vector3 wordPos;
+		if (Physics.Raycast (ray, out hit, 1000f)) {
+			
+			wordPos = hit.point;
+			
+		} else {
+			
+			wordPos = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);
+		}
+		
+		return wordPos;
+	}
+
+	public void SetCoordinatesOnInputField (double latitude, double longitude)
+	{
+		DirectionInputField.text = latitude.ToString () + " , " + longitude.ToString ();
 	}
 }
